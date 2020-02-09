7d3f8bb6d1bee13f7503cc29ade229ac0ec4b295
Satyam <satyam@satyam.com.ar>
2020-02-09 18:29:01 +0100

    Autito-remote changed to use SoftTimer

---
+++ autito-remote/autito-remote.h
@@ -0,0 +1,91 @@
+#ifndef AUTITO_REMOTE_H
+#define AUTITO_REMOTE_H
+
+#include <SoftTimer.h>
+#include <CurrentSensor.h>
+#include <Potenciometro.h>
+#include <Debouncer.h>
+#include <PciManager.h>
+#include <TunePlayer.h>
+#include <Servo.h>
+#include <pitches.h>
+
+#define DEBOUNCE_INTERVAL 25
+
+#define MAX_BYTE 255
+
+// Joystick
+#define JOY_SWITCH 2
+#define JOY_X A1
+#define JOY_Y A0
+
+void jXHandler(int x);
+void jYHandler(int y);
+
+// DC Motor
+#define ENABLE 5
+#define DIRA 3
+#define DIRB 4
+#define MAX_SPEED 255
+
+void setMotor(int speed);
+
+// Servo
+#define SERVO 9
+#define MAX_SERVO 90
+
+void setTurn(int turn);
+
+// Music:
+#define BUZZER 8
+#define DURATION 300
+#define GAP (DURATION + 50)
+
+int tune[] = {
+  NOTE_AS4, 1,
+  NOTE_C5,  1,
+  NOTE_GS4, 1,
+  NOTE_GS3, 1,
+  NOTE_DS4, 3,
+  0  
+};
+void startTune();
+void tuneEndHandler();
+
+// Current Sensor
+#define CURRENT_SENSOR A2
+void csHandler(float value);
+
+// Remote commands
+
+#define GO_FORWARD 'f'
+#define GO_BACK 'b'
+#define STOP 'S'
+
+#define FULL_FORWARD 'F'
+#define FULL_BACK 'B'
+
+#define TURN_LEFT 'l'
+#define TURN_RIGHT 'r'
+#define GO_STRAIGHT '|'
+
+#define BEEP '!'
+
+#define LED '#'
+
+#define REMOTE 'R'
+
+void cmdHandler(Task* me);
+
+// Report Status
+
+#define REMOTE_CMD '>'
+#define SPEED 's'
+#define TURN 't'
+#define CURRENT 'c'
+#define X_POS 'x'
+#define Y_POS 'y'
+
+#define REPORT(cmd, value)  Serial.print(cmd);Serial.println(value)
+
+#endif
--- autito-remote/autito-remote.ino
+++ autito-remote/autito-remote.ino
@@ -1,71 +1,153 @@
-#include <Bounce2.h>
-#include <Servo.h>
-#include <pitches.h>
+#include "autito-remote.h";
 
-#define DEBOUNCE_INTERVAL 25
+Potenciometro jX(JOY_X, &jXHandler);
+Potenciometro jY(JOY_Y, &jYHandler);
+Debouncer jSwDebouncer(JOY_SWITCH, MODE_CLOSE_ON_PUSH, startTune, NULL, true);
 
-#define WAIT 10
-
-#define MAX_BYTE 255
-
-// Joystick
-#define JOY_SWITCH 2
-#define JOY_X A0
-#define JOY_Y A1
-
-// DC Motor
-#define ENABLE 5
-#define DIRA 3
-#define DIRB 4
-#define CURRENT_SENSOR A2
-#define MAX_SPEED 255
-
-// Servo
-#define SERVO 9
-#define MAX_SERVO 90
+CurrentSensor currentSensor(CURRENT_SENSOR, &csHandler);
 
 Servo servo;
-Bounce joyButton = Bounce();
 
-// Music:
-#define BUZZER 8
-#define DURATION 300
-#define GAP (DURATION + 50)
+Task fetchCmd(1, cmdHandler);
 
-int melody[] = {
-  NOTE_AS4, NOTE_C5, NOTE_GS4, NOTE_GS3, NOTE_DS4
-};
+void setup() {
+  Serial.begin(9600);
 
-// Remote commands
-
-#define GO_FORWARD 'f'
-#define GO_BACK 'b'
-#define STOP 'S'
-
-#define FULL_FORWARD 'F'
-#define FULL_BACK 'B'
-
-#define TURN_LEFT 'l'
-#define TURN_RIGHT 'r'
-#define GO_STRAIGHT '|'
-
-#define BEEP '!'
-
-#define LED '#'
-
-#define REMOTE 'R'
+  // Joystick
+  jX.start();
+  jY.start();
+  jSwDebouncer.init();
+  PciManager.registerListener(JOY_SWITCH, &jSwDebouncer);
 
 
-// Variables to hold values for centered joystick
-int centerX;
-int centerY;
-// Variables to hold last known state of joystick
-int lastX;
-int lastY;
+  // DC Motor
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
 
-int readJoystick(int pin, int center) {
-  return (analogRead(pin) >> 1) - center;
+
+  // Servo
+  servo.attach(SERVO);
+  setTurn(0);
+
+  // Built in LED
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Remote command reader
+  SoftTimer.add(&fetchCmd);
+
+  // Tune player
+
+  TunePlayer.attach(BUZZER);
+  TunePlayer.onEnd(&tuneEndHandler);
+
+  // Play it slow
+  TunePlayer.setNoteDuration(DURATION);
+  TunePlayer.setGap(GAP);
+
+  // Current sensor
+
+  // Make it slow right now so it doesn't overwhelm the logger.
+  currentSensor.setPollingInterval(50);
+  currentSensor.setTotalCount(20);
+// Hold it at this point
+  // currentSensor.start();
+
+  Serial.println("s0t0!0#0");
+
 }
+
+
+void cmdHandler(Task* me) {
+  byte firstByte = 0;
+  if (Serial.available()) {
+    byte remoteCommand = (byte)Serial.read();
+    REPORT(REMOTE_CMD, char(remoteCommand));
+    switch (remoteCommand) {
+
+      case GO_FORWARD:
+        firstByte = (byte)Serial.read();
+        setMotor(firstByte);
+
+        REPORT(SPEED, firstByte);
+        break;
+
+      case GO_BACK:
+        firstByte = (byte)Serial.read();
+        setMotor(-firstByte);
+
+        REPORT(SPEED, -firstByte);
+        break;
+
+      case STOP:
+        setMotor(0);
+
+        REPORT(SPEED, 0);
+        break;
+
+      case TURN_LEFT:
+        firstByte = (byte)Serial.read();
+        setTurn(-firstByte);
+
+        REPORT(TURN, -firstByte);
+        break;
+
+      case TURN_RIGHT:
+        firstByte = (byte)Serial.read();
+        setTurn(firstByte);
+
+        REPORT(TURN, firstByte);
+        break;
+
+      case GO_STRAIGHT:
+        setTurn(0);
+
+        REPORT(TURN, 0);
+        break;
+
+      case BEEP:
+        startTune();
+        break;
+
+      case LED:
+        firstByte = (byte)Serial.read();
+        digitalWrite(LED_BUILTIN, firstByte ? HIGH : LOW );
+
+        REPORT(LED, firstByte);
+        break;
+      case FULL_FORWARD:
+        digitalWrite(DIRA, LOW);
+        digitalWrite(DIRB, HIGH);
+
+        digitalWrite(ENABLE, HIGH);
+        REPORT(SPEED, 255);
+        break;
+      case FULL_BACK:
+        digitalWrite(DIRA, HIGH);
+        digitalWrite(DIRB, LOW);
+
+        digitalWrite(ENABLE, HIGH);
+        REPORT(SPEED, -255);
+        break;
+      case REMOTE:
+        bool remote = (bool)Serial.read();
+        REPORT(REMOTE, remote);
+
+        if (remote) {
+          jX.stop();
+          jY.stop();
+        } else {
+          jX.start();
+          jY.start();
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
 
 void setMotor(int speed)  {
   if (speed < 0) {
@@ -84,243 +166,33 @@ void setTurn(int turn) {
   servo.write(long(turn) * MAX_SERVO / MAX_BYTE + MAX_SERVO);
 }
 
-// Play tune
-#define QUIET 1
-#define START_PLAYING 2
-#define PLAYING 3
-
-int note = 0;
-int playing = QUIET;
-unsigned long timeForNextNote;
-
-
-void setup() {
-  Serial.begin(9600);
-
-  // Joystick
-
-  joyButton.attach(JOY_SWITCH, INPUT_PULLUP);
-  joyButton.interval(DEBOUNCE_INTERVAL);
-
-  // DC Motor
-  pinMode(ENABLE, OUTPUT);
-  pinMode(DIRA, OUTPUT);
-  pinMode(DIRB, OUTPUT);
-
-  // Read the values for the joystick at idle (center) position
-  // Values scaled from 0 to 1025 into 0 to 512 so they will
-  centerX = readJoystick(JOY_X, 0);
-  centerY = readJoystick(JOY_Y, 0);
-
-
-  // Servo
-  servo.attach(SERVO);
-  setTurn(0);
-
-  // Built in LED
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  Serial.println("s0t0!0#0");
-
+void csHandler(float value) {
+  REPORT(CURRENT, value);
 }
 
-byte remoteCommand = 0;
-byte firstByte = 0;
-
-bool remote = false;
-
-long idleCurrent = 0;
-
-long sum = 0;
-int cnt = 0;
-
-void loop() {
-
-  if (cnt < 10) {
-    sum += analogRead(CURRENT_SENSOR);
-    cnt++;
-  } else {
-    if (idleCurrent) {
-      if (Serial.availableForWrite() > 6) {
-        Serial.print('&');
-        Serial.print(idleCurrent);
-        Serial.print('c');
-        Serial.println((sum - idleCurrent));
-        cnt = 0;
-        sum = 0;
-      }
-    } else {
-      idleCurrent = sum;
-      cnt = 0;
-      sum = 0;
-    }
-  }
-
-
-
-
-  if (Serial.available()) {
-    remoteCommand = (byte)Serial.read();
-    Serial.print('>');
-    Serial.print(char(remoteCommand));
-    switch (remoteCommand) {
-
-      case GO_FORWARD:
-        firstByte = (byte)Serial.read();
-        setMotor(firstByte);
-
-        Serial.print('s');
-        Serial.println(firstByte);
-        break;
-
-      case GO_BACK:
-        firstByte = (byte)Serial.read();
-        setMotor(-firstByte);
-
-        Serial.print('s');
-        Serial.println(-firstByte);
-        break;
-
-      case STOP:
-        setMotor(0);
-
-        Serial.print('s');
-        Serial.println(0);
-        break;
-
-      case TURN_LEFT:
-        firstByte = (byte)Serial.read();
-        setTurn(-firstByte);
-
-        Serial.print('t');
-        Serial.println(-firstByte);
-        break;
-
-      case TURN_RIGHT:
-        firstByte = (byte)Serial.read();
-        setTurn(firstByte);
-
-        Serial.print('t');
-        Serial.println(firstByte);
-        break;
-
-      case GO_STRAIGHT:
-        setTurn(0);
-
-        Serial.print('t');
-        Serial.println(0);
-        break;
-
-      case BEEP:
-        if (playing == QUIET) {
-          playing = START_PLAYING;
-
-          Serial.print('!');
-          Serial.println(true);
-        }
-        break;
-
-      case LED:
-        firstByte = (byte)Serial.read();
-        digitalWrite(LED_BUILTIN, firstByte ? HIGH : LOW );
-
-        Serial.print('#');
-        Serial.println(firstByte);
-        break;
-      case FULL_FORWARD:
-        digitalWrite(DIRA, LOW);
-        digitalWrite(DIRB, HIGH);
-
-        digitalWrite(ENABLE, HIGH);
-        Serial.println("s255");
-        break;
-      case FULL_BACK:
-        digitalWrite(DIRA, HIGH);
-        digitalWrite(DIRB, LOW);
-
-        digitalWrite(ENABLE, HIGH);
-        Serial.println("s-255");
-        break;
-      case REMOTE:
-        firstByte = (byte)Serial.read();
-        remote = firstByte;
-
-        Serial.print('R');
-        Serial.println(firstByte);
-
-
-      default:
-        break;
-    }
-  }
-
-  if (!remote) {
-
-    joyButton.update();
-    if ( joyButton.fell() ) {
-      if (playing == QUIET) {
-        playing = START_PLAYING;
-        Serial.println("!1");
-      }
-    }
-
-    int y = readJoystick(JOY_Y, centerY);
-
-    // Check for significant movement on Y axis
-    if (y != lastY) {
-      lastY = y;
-
-      // The minus sigh reflects the orientation of the joystick.
-      int s = - min(y, MAX_BYTE);
-      setMotor(s);
-
-      Serial.print('s');
-      Serial.print(s);
-      Serial.print('y');
-      Serial.println(s);
-    }
-
-    int x = readJoystick(JOY_X, centerX);
-    // Check for significant movement on X axis
-    if (x != lastX) {
-      lastX = x;
-
-      int a = min(x, MAX_BYTE);
-
-      setTurn(a);
-
-      Serial.print('t');
-      Serial.print(a);
-      Serial.print('x');
-      Serial.println(a);
-    }
-  }
-  // Check on tune playing
-  switch (playing) {
-
-    case START_PLAYING:
-      playing = PLAYING;
-      note = 0;
-      timeForNextNote = millis() + GAP;
-      tone(BUZZER, melody[note], DURATION);
-      break;
-
-    case PLAYING:
-      if (millis() > timeForNextNote) {
-        note++;
-        if (note == 4) {
-          playing = QUIET;
-          tone(BUZZER, melody[note], 3 * DURATION);
-          Serial.println("!0");
-        } else {
-          tone(BUZZER, melody[note], DURATION);
-          timeForNextNote = millis() + GAP;
-        }
-      }
-      break;
-      // No need to do anything for playing == QUIET
-  }
-
-  // wait for a while
-  delay(WAIT);
+void startTune() {
+  REPORT(BEEP, true);
+  TunePlayer.play(tune);
 }
+
+void tuneEndHandler() {
+  REPORT(BEEP, false);
+};
+
+void jXHandler(int x) {
+  int a = min(x, MAX_BYTE);
+
+  setTurn(a);
+
+  REPORT(TURN, a);
+  REPORT(X_POS, a);
+
+};
+void jYHandler(int y) {
+  int s = - min(y, MAX_BYTE);
+  setMotor(s);
+  REPORT(SPEED, s);
+  REPORT(Y_POS, s);
+  
+};
+
